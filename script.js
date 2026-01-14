257648e6b3c6208089983db9841cf9cee92a33ab
Tobin Smith <tobinleesmith@gmail.com>
2026-01-14 13:48:36 -0500

    finished adding comments to script

--- script.js
+++ script.js
@@ -213,66 +213,109 @@ const map = L.map('map', {
 // set view and zoom to correct area so (most of) the points are in frame when the map loads
 }).setView([37, 28], 6);
 
+// create Open Street Map Layer from url
 const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  // proper attribution
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// add OSM layer to the map layer
 }).addTo(map);
 
+// create Consortium of Ancient World Mappers layer (better for a map of the ancient world)
 const overlay = L.tileLayer('https://cawm.lib.uiowa.edu/tiles/{z}/{x}/{y}.png', {
+  // proper attribution
   attribution: '&copy; <a href="https://cawm.lib.uiowa.edu/index.html">Consortium of Ancient World Mappers</a>'
+// add CAWM layer to the map layer (after adding OSM layer so CAWM is on top)
 }).addTo(map);
 
+// create icon for bishops that have a location (non-rural)
 const locatedIcon = L.icon({
-    iconUrl: 'locatedIcon.png',
-    iconAnchor: [9,9]
+  // pull icon source  
+  iconUrl: 'locatedIcon.png',
+  // make symbol centered
+  iconAnchor: [9,9]
 });
 
+// create icon for rural bishops
 const ruralIcon = L.icon({
-    iconUrl: 'ruralIcon.png',
-    iconAnchor: [9,9]
+  // pull icon source
+  iconUrl: 'ruralIcon.png',
+  // make symbol centered
+  iconAnchor: [9,9]
 });
 
+// create icon for Constantinople
 const starIcon = L.icon({
+  // pull icon source
   iconUrl: 'starIcon.png',
+  // make symbol centered
   iconAnchor: [12, 11.5]
 })
 
+// create empty array for names of eparchies to be placed in 
 var eparchyList = [];
+// create iterator to run through each bishop in the jeoGSON feature from the QGIS data
 for (var i = 0; i < geojsonFeature.features.length; i++) {
+  // grab the eparchy from the ith bishop in the data
   let current_eparchy = geojsonFeature.features[i].properties.eparchy;
+  // if that eparchy is not in the array for the names of eparchies,
   if (!eparchyList.includes(current_eparchy)) {
+    // add it to the array
     eparchyList.push(current_eparchy);
   }
 };
 
-var eparchyLayers = [];
+// create empty object for layer objects that will eventually hold all datapoints within one eparchy
+var eparchyLayers = {};
+// create an iterator to run through each eparchy in the list of eparchy names
 for (var i = 0; i < eparchyList.length; i++) {
+  // create layer object from geoJSON data
   var eparchyBishops = L.geoJSON(geojsonFeature, {
-      filter: function (feature, layer) {
-          return (feature.properties.eparchy === eparchyList[i]);
-      },
-      pointToLayer: function(feature, latlng) {
-          if (feature.properties.rural === 1.0) {
-            return L.marker(latlng, {
-              icon: ruralIcon
-          })} else {
-            return L.marker(latlng, {
-              icon: locatedIcon
-          })};
+    // filter data using a function
+    filter: function (feature, layer) {
+      /*  make the functionreturn t/f depending on if the bishop's eparchy matches the ith eparchy. 
+          this makes it so only bishops in the ith eparchy are included in the layer being created
+      */  
+      return (feature.properties.eparchy === eparchyList[i]);  
+    },
+    /*  pointToLayer determines how points in a geoJSON feature show up on the map. 
+        it wants a function that returns a marker object
+    */      
+    pointToLayer: function(feature, latlng) {
+      // if the feature is a rural bishop   
+      if (feature.properties.rural === 1.0) {
+        // return a marker object at the bishop's coordinates     
+        return L.marker(latlng, {
+          // give it the rural icon
+          icon: ruralIcon 
+        })
+      // otherwise (if the feature is not a rural bishop)  
+      } else {
+        // return a marker object at the bishop's coordinates    
+        return L.marker(latlng, {
+          // give it the located icon    
+          icon: locatedIcon          
+        })};
       }
-      }).bindPopup(function (layer) {
-        return layer.feature.properties.name+" of "+layer.feature.properties.city+"<br />Eparchy of "+layer.feature.properties.eparchy;
-      }).addTo(map);
-  eparchyLayers[eparchyList[i]] = eparchyBishops;
+    // add a popup to each feature  
+    }).bindPopup(function (layer) {
+      /*  make the text in the popup include the bishop name, city, and eparchy: 
+          BISHOP of CITY
+          Eparchy of EPARCHY
+      */  
+      return layer.feature.properties.name+" of "+layer.feature.properties.city+"<br />Eparchy of "+layer.feature.properties.eparchy;
+    // add popups to the map  
+    }).addTo(map);
+    // put the layer object in the eparchyLayers object with a key corresponding to the correct eparchy
+    eparchyLayers[eparchyList[i]] = eparchyBishops;
 }
 
+// create interactive menu to switch between base layers
 var baseLayers = {
+  // add OSM layer
   "OpenStreetMap": osm,
+  // add CAWM layer
   "Ancient World": overlay
 };
 
+// create layer control menu to select eparchies to show on the map
 var layercontrol = L.control.layers(baseLayers, eparchyLayers).addTo(map);
-
-var tooltip = L.tooltip()
-    .setLatLng([37, 28])
-    .setContent(eparchyLayers[0].toString())
-    .addTo(map);
